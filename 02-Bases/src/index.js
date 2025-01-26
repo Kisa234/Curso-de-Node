71e0ba5189a84090223fdd6a5a2e622c53dce59c
Kisa234 <110016263+Kisa234@users.noreply.github.com>
2025-01-26 18:08:08 -0500

    update progress

--- 02-Bases/src/index.js
+++ 02-Bases/src/index.js
@@ -2,18 +2,26 @@
 // require('./js-foundation/02-destructuring');
 // console.log (emailTemplate);
 // const{getUserById}=require('./js-foundation/04-arrow');
-const getPokemonById = require('./js-foundation/06-promises');
+// const getPokemonById = require('./js-foundation/06-promises');
+const {buildLogger} = require('../plugins/winston.plugin');
+
+const logger = buildLogger('index');
+logger.log('Hello from index');
+logger.error('Esto es algo malo')
 
 
+
+
+
+
+// getPokemonById(4)
+//     .then( (pokemon) => console.log({pokemon}) )
+//     .catch( (err) =>  console.log('Porfavor intenta de nuevo'))
+//     .then( () => console.log('Finalmente'));
 
 // getPokemonById(4, (pokemon) => {
 //     console.log({pokemon});
 // });
-
-getPokemonById(4)
-    .then( (pokemon) => console.log({pokemon}) )
-    .catch( (err) =>  console.log('Porfavor intenta de nuevo'))
-    .then( () => console.log('Finalmente'));
 
 // !REFERENCIA A LA FUNCION FACTORY
 // const {getAge, getUUID} = require('../plugins');
