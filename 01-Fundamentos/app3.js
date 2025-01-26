31b9ae338695101dc3fc0f613024ca14916bef0b
Kisa234 <110016263+Kisa234@users.noreply.github.com>
2025-01-26 00:57:39 -0500

    update progress

---
+++ 01-Fundamentos/app3.js
@@ -0,0 +1,11 @@
+const fs =  require ('fs');
+
+const content =  fs.readFileSync('README.md', 'utf8');
+
+const wordCount = content.split(' ').length;
+const reactWordCount = content.match(/react/gi).length;
+
+
+
+console.log('Palabras:', wordCount);
+console.log('Palabras React:', reactWordCount);
